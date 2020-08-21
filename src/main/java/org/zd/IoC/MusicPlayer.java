23c7b19d7126b20c41f8fefdd69b2bb0a7fea0a4
zdezak <zdezarus@gmail.com>
2020-08-21 15:51:23 +0500

    Пустой конструктор и инициализация Обекта через property

--- src/main/java/org/zd/IoC/MusicPlayer.java
+++ src/main/java/org/zd/IoC/MusicPlayer.java
@@ -2,9 +2,13 @@ package org.zd.IoC;
 
 public class MusicPlayer {
     private Music music;
-    public MusicPlayer(Music music){
+    public MusicPlayer(){
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
+
     public void playMusic(){
         System.out.println("playing: "+music.getSong());
     }
