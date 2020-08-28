54d1a63afa41390fa6245f983fc11d394713ff74
zdezak <zdezarus@gmail.com>
2020-08-28 15:11:38 +0500

    Аннотация @Qualifier

--- src/main/java/org/zd/IoC/MusicPlayer.java
+++ src/main/java/org/zd/IoC/MusicPlayer.java
@@ -1,19 +1,22 @@
 package org.zd.IoC;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic music;
-    private RockMusic music2;
+    private Music music1;
+    private Music music2;
     @Autowired
-    public MusicPlayer(ClassicalMusic music, RockMusic music2){
-        this.music =music;
-        this.music2 = music2;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1
+            ,@Qualifier("classicalMusic")Music music2){
+        this.music2 =music2;
+        this.music1 = music1;
     }
 
     public String playMusic(){
-        return "playing: "+music.getSong();
+        return "playing: "+music1.getSong()
+                +". Next "+ music2.getSong();
     }
 }
